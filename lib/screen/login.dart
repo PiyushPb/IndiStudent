4cb9595630aeb71b5d6c229d1c5fb2654b6e1b7c
Piyush Pardeshi <piyushp0512@gmail.com>
2023-02-12 23:41:24 +0530

    welcome screen responsive added email screen responsive added

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -16,81 +16,192 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        // mobile code
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: 100,
-                child: Image.asset('assets/images/logo.png'),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              SizedBox(
-                child: Image.asset('assets/images/communityimg.png'),
-              ),
-              const SizedBox(height: 20.0),
-              const Text(
-                "Join India's most active student community with 1.5 million+ students",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            if (constraints.maxWidth > 820) {
+              return desktop_login_form();
+            } else {
+              return mobile_login_form();
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  Widget desktop_login_form() {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/images/magicpattern-ixxjruC7Gg4-unsplash.jpg',
                 ),
+                fit: BoxFit.cover,
               ),
-              const SizedBox(height: 20.0),
-              const Text(
-                  'Make smart friends, learn new skills, and grow your network like you have never done before. It\'s a place to have fun and explore more students with similar interests.',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 83, 83, 83),
-                  )),
-              Expanded(child: Container()),
-              SizedBox(
-                width: double.infinity,
-                height: 50.0,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const LoginWithEmailPage(),
-                      ),
-                    );
-                  },
-                  child: const Text('Sign in using Email'),
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              // ignore: prefer_const_constructors
-              Align(
-                alignment: Alignment.center,
-                child: const Text(
-                  'OR',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 83, 83, 83),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              SizedBox(
-                width: double.infinity,
-                height: 50.0,
-                child: OutlinedButton.icon(
-                  onPressed: () {},
-                  // ignore: prefer_const_constructors
-                  icon: Image.asset(
-                    'assets/images/google.png',
-                    height: 24,
-                  ),
-                  label: Text('Sign in using Google'), // <-- Text
-                ),
-              ),
-            ],
+            ),
           ),
         ),
+        Expanded(
+          child: Center(
+            child: SizedBox(
+              width: 600,
+              child: Padding(
+                padding: const EdgeInsets.all(70.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 200,
+                      child: Image.asset('assets/images/logo.png'),
+                    ),
+                    Expanded(child: Container()),
+                    SizedBox(
+                      child: Image.asset('assets/images/communityimg.png'),
+                    ),
+                    const SizedBox(height: 20.0),
+                    const Text(
+                      "Join India's most active student community with 1.5 million+ students",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(height: 20.0),
+                    const Text(
+                        'Make smart friends, learn new skills, and grow your network like you have never done before. It\'s a place to have fun and explore more students with similar interests.',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 83, 83, 83),
+                        )),
+                    Expanded(child: Container()),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50.0,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const LoginWithEmailPage(),
+                            ),
+                          );
+                        },
+                        child: const Text('Sign in using Email'),
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    // ignore: prefer_const_constructors
+                    Align(
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'OR',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 83, 83, 83),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50.0,
+                      child: OutlinedButton.icon(
+                        onPressed: () {},
+                        // ignore: prefer_const_constructors
+                        icon: Image.asset(
+                          'assets/images/google.png',
+                          height: 24,
+                        ),
+                        label: Text('Sign in using Google'), // <-- Text
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  Widget mobile_login_form() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 100,
+            child: Image.asset('assets/images/logo.png'),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          SizedBox(
+            child: Image.asset('assets/images/communityimg.png'),
+          ),
+          const SizedBox(height: 20.0),
+          const Text(
+            "Join India's most active student community with 1.5 million+ students",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(height: 20.0),
+          const Text(
+              'Make smart friends, learn new skills, and grow your network like you have never done before. It\'s a place to have fun and explore more students with similar interests.',
+              style: TextStyle(
+                color: Color.fromARGB(255, 83, 83, 83),
+              )),
+          Expanded(child: Container()),
+          SizedBox(
+            width: double.infinity,
+            height: 50.0,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LoginWithEmailPage(),
+                  ),
+                );
+              },
+              child: const Text('Sign in using Email'),
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          // ignore: prefer_const_constructors
+          Align(
+            alignment: Alignment.center,
+            child: const Text(
+              'OR',
+              style: TextStyle(
+                color: Color.fromARGB(255, 83, 83, 83),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          SizedBox(
+            width: double.infinity,
+            height: 50.0,
+            child: OutlinedButton.icon(
+              onPressed: () {},
+              // ignore: prefer_const_constructors
+              icon: Image.asset(
+                'assets/images/google.png',
+                height: 24,
+              ),
+              label: Text('Sign in using Google'), // <-- Text
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screen/login_pages/login_with_email.dart
+++ lib/screen/login_pages/login_with_email.dart
@@ -29,94 +29,212 @@ class _LoginWithEmailPageState extends State<LoginWithEmailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            if (constraints.maxWidth > 600) {
+              return desktop_screen();
+            } else {
+              return mobile_screen();
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget desktop_screen() {
+    return Container(
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/magicpattern-ixxjruC7Gg4-unsplash.jpg',
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: SizedBox(
+                width: 600,
+                child: Padding(
+                  padding: const EdgeInsets.all(60.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Expanded(child: Container()),
+                      const Text(
+                        'Login with Email',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      const SizedBox(height: 100),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        // ignore: prefer_const_literals_to_create_immutables
+                        children: [
+                          Form(
+                            autovalidateMode: AutovalidateMode.always,
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                  child: TextFormField(
+                                    validator: validateEmail,
+                                    // ignore: prefer_const_constructors
+                                    decoration: InputDecoration(
+                                      filled: true,
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      // ignore: prefer_const_constructors
+                                      labelText: 'Email',
+                                      hintText: 'Email',
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          const Text(
+                            'We will send you a link to this email. You can just tap on the link to login.',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 56, 56, 56),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          TextButton(
+                            style: ButtonStyle(
+                              overlayColor:
+                                  MaterialStateProperty.all(Colors.transparent),
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('Go back to google login'),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 100),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50.0,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            // primary: Color.fromARGB(255, 7, 38, 53),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Text('Continue'),
+                        ),
+                      ),
+                      Expanded(child: Container()),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget mobile_screen() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          IconButton(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            padding: EdgeInsets.zero,
+            constraints: BoxConstraints(),
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'Login with Email',
+            style: TextStyle(fontSize: 20),
+          ),
+          Expanded(child: Container()),
+          Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
             // ignore: prefer_const_literals_to_create_immutables
             children: [
-              IconButton(
-                splashColor: Colors.transparent,
-                highlightColor: Colors.transparent,
-                padding: EdgeInsets.zero,
-                constraints: BoxConstraints(),
-                icon: Icon(Icons.arrow_back),
+              Form(
+                autovalidateMode: AutovalidateMode.always,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      child: TextFormField(
+                        validator: validateEmail,
+                        // ignore: prefer_const_constructors
+                        decoration: InputDecoration(
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          // ignore: prefer_const_constructors
+                          labelText: 'Email',
+                          hintText: 'Email',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'We will send you a link to this email. You can just tap on the link to login.',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 56, 56, 56),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextButton(
+                style: ButtonStyle(
+                  overlayColor: MaterialStateProperty.all(Colors.transparent),
+                ),
                 onPressed: () {
                   Navigator.pop(context);
                 },
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Login with Email',
-                style: TextStyle(fontSize: 20),
-              ),
-              Expanded(child: Container()),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  Form(
-                    autovalidateMode: AutovalidateMode.always,
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          child: TextFormField(
-                            validator: validateEmail,
-                            // ignore: prefer_const_constructors
-                            decoration: InputDecoration(
-                              filled: true,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              // ignore: prefer_const_constructors
-                              labelText: 'Email',
-                              hintText: 'Email',
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    'We will send you a link to this email. You can just tap on the link to login.',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 56, 56, 56),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  TextButton(
-                    style: ButtonStyle(
-                      overlayColor:
-                          MaterialStateProperty.all(Colors.transparent),
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text('Go back to google login'),
-                  ),
-                ],
-              ),
-              Expanded(child: Container()),
-              SizedBox(
-                width: double.infinity,
-                height: 50.0,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    // primary: Color.fromARGB(255, 7, 38, 53),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: const Text('Continue'),
-                ),
+                child: Text('Go back to google login'),
               ),
             ],
           ),
-        ),
+          Expanded(child: Container()),
+          SizedBox(
+            width: double.infinity,
+            height: 50.0,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                // primary: Color.fromARGB(255, 7, 38, 53),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {},
+              child: const Text('Continue'),
+            ),
+          ),
+        ],
       ),
     );
   }
